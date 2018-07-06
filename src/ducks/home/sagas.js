1b303d585ccef4b2a5dfc3ea8cbf70bd84ca6cfc
dreamingblackcat <dreamingblackcat10@gmail.com>
2018-07-06 16:53:55 +0630

    Save historical record in firebase

--- src/ducks/home/sagas.js
+++ src/ducks/home/sagas.js
@@ -1,10 +1,10 @@
-import { call, put } from 'redux-saga/effects';
+import { call, put, all } from 'redux-saga/effects';
 import * as constants from './constants';
 import firebase from '../../firebase';
 
 export function* fetchUserData(action) {
   try {
-    const ref = firebase.database().ref(`users/${action.user.uid}`);
+    const ref = firebase.database().ref(`users/${action.user.uid}/latest`);
     const snapshot = yield call([ref, ref.once], 'value');
     console.log('Snapshot', snapshot);
     const data = snapshot.val();
@@ -22,9 +22,10 @@ export function* fetchUserData(action) {
 
 export function* saveUserData(action) {
   try {
-    const ref = firebase.database().ref(`users/${action.user.uid}`);
-    const updates = { ...action.data, lastSubmittedAt: new Date() };
-    yield call([ref, ref.set], updates);
+    const ref = firebase.database().ref(`users/${action.user.uid}/latest`);
+    const historyRef = firebase.database().ref(`users/${action.user.uid}/history`);
+    const updates = { ...action.data, lastSubmittedAt: firebase.database.ServerValue.TIMESTAMP };
+    yield all([call([ref, ref.set], updates), call([historyRef, historyRef.push], updates)]);
     yield put({
       type: constants.SAVE_SUCCESSFUL,
       data: updates,
